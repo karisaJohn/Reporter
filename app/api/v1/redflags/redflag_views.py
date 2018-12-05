4bc8017ac0c9adde3a4ed8cfc1057c8d1ecd4f04
karisaJohn <johnkahindi96@gmail.com>
2018-12-05 17:59:39 +0300

    removed hard coded data in models and made changes to the test file

--- app/api/v1/redflags/redflag_views.py
+++ app/api/v1/redflags/redflag_views.py
@@ -10,20 +10,28 @@ class RedFlags(Resource, RedFlagsModel):
         self.db = RedFlagsModel()
 
     def post(self):
+        data_json = request.get_json(force=True)
+        if not data_json:
+            return {'status' : 200, "Message" : "No input data provided"}
         data = request.get_json()
         createdBy = data['createdBy']
         location = data['location']
         title = data['title']
 
-        resp = self.db.save(createdBy, location, title)
-        return make_response(jsonify({"Users":resp}),201)
+        self.db.save(createdBy, location, title)
+        return make_response(jsonify({
+            "Message": "RedFlag added successfully."
+            }),201)
 
     def get(self):
-        resp = self.db.get_redflags()
-        return make_response(jsonify(
-            {
-                "Users":resp,
-                "status" : 200
+        redflags = self.db.get_redflags()
+        if redflags == None:
+            return make_response(jsonify({
+                "Message" : "No records found."
+            }))
+        return make_response(jsonify({
+                "Records": redflags,
+                "Status" : 200
             }),200)
 
 
@@ -32,13 +40,51 @@ class RedFlag(Resource, RedFlagsModel):
         self.db = RedFlagsModel()
 
     def get(self, num):
-        resp = self.db.get_redflag(num)
-        return make_response(jsonify({"Users":resp}),200)
+        redflag = self.db.search(num)
+        if redflag == None:
+            return make_response(jsonify({
+                "Error" : "RedFlag does not exit."
+            }), 404)
+        return make_response(jsonify({
+            "Record" : redflag
+        }), 200)
     
     def patch(self, num):
-        resp = self.db.edit_redflag(num)
-        return make_response(jsonify({"Redflag":resp}),200)
+        redflag = self.db.search(num)
+        if redflag == None:
+            return make_response(jsonify({
+                "Error" : "RedFlag does not exist."
+            }), 404)
+
+        elif redflag["status"] == "Draft":
+            self.db.edit_redflag
+            return make_response(jsonify({
+                "Message" : "Change made successfully."
+            }), 200)
+
+        elif redflag["status"] == "Under Investigation":
+            return make_response(jsonify({
+                "Message" : "RedFlag still under investigation."
+            }), 404)
+
+        elif redflag["status"] == "Rejected":
+            return make_response(jsonify({
+                "Message" : "RedFlag was rejected."
+            }), 404)
+        
+        elif redflag["status"] == "Resolved":
+            return make_response(jsonify({
+                "Status" : 404,
+                "Message" : "RedFlag already resolved."
+            }), 404)
     
     def delete(self, num):
-        self.db.delete_redflag(num)
-        return {"Message":"Red-Flag deleted successfully"}
+        redflag = self.db.search(num)
+        if redflag == None:
+            return make_response(jsonify({
+                "Error" : "RedFlag does not exist."
+            }), 404)
+        self.db.delete_redflag(redflag)
+        return make_response(jsonify({
+            "Data" : "Record deleted successfully."
+            }), 200)
